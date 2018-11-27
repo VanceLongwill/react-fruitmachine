640dc79ee8035e0418c81e49fbaa0f17b3d4c421
Stan Day <stand@whenyoumove.com>
2018-11-27 16:29:50 +0000

    remove reference to image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App">Hey</div>;
+    return <div className="App">Slot machine</div>;
   }
 }
 
