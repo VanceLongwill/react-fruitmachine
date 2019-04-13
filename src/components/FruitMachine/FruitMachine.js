49c4d5be685f14b60cf1df8041e06ba9c951062b
Vance Longwill <vancelongwill@gmail.com>
2019-04-13 23:33:45 +0300

    feat: deterministic spin animation

--- src/components/FruitMachine/FruitMachine.js
+++ src/components/FruitMachine/FruitMachine.js
@@ -1,47 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 import Wheel from '../Wheel'
 
 import styles from './FruitMachine.module.css'
 
-function usePauseState(delay) {
-  const [ isPaused, setPause ] = useState(true);
-
-  const setPauseWrapper = () => {
-    if (!isPaused) {
-      if (delay) {
-        const t = setTimeout(() => setPause(true), delay)
-        return () => clearTimeout(t)
-      }
-      setPause(true)
-      return
-    }
-    setPause(false)
-  }
-
-  return [ isPaused, setPauseWrapper ] 
-}
-
 export function FruitMachine(props) {
-  const [isFirstWheelPaused, setFirstWheelPause] = usePauseState(0)
-  const [isSecondWheelPaused, setSecondWheelPause] = usePauseState(300)
-  const [isThirdWheelPaused, setThirdWheelPause] = usePauseState(600)
-
+  const [isPaused, setPause] = useState(true)
 
   return (
     <div>
       <button onClick={() => {
-        setFirstWheelPause()
-        setSecondWheelPause()
-        setThirdWheelPause()
+        setPause(!isPaused)
       }}>
         Click me
       </button>
       <div className={styles.stage}>
         <div className={styles.rotate}>
-          <Wheel isPaused={isFirstWheelPaused} delay={0}/>
-          <Wheel isPaused={isSecondWheelPaused} delay={300}/>
-          <Wheel isPaused={isThirdWheelPaused} delay={600}/>
+          <Wheel isPaused={isPaused} prevIndex={0} nextIndex={11} delay={600}/>
+          <Wheel isPaused={isPaused} prevIndex={0} nextIndex={1} delay={300}/>
+          <Wheel isPaused={isPaused} prevIndex={0} nextIndex={2} delay={0}/>
         </div>
       </div>
     </div>
--- src/components/Wheel/Wheel.js
+++ src/components/Wheel/Wheel.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import cx from 'classnames'
+import styled, { keyframes } from 'styled-components'
 
 import WheelFace from '../WheelFace'
-import styles from './Wheel.module.css'
 
 const WHEELFACES_PER_ROW = 12
 const WHEEL_RADIUS = 200
@@ -10,18 +9,42 @@ const wheelAngle = 360 / WHEELFACES_PER_ROW
 
 const colors = ['red', 'green', 'blue', 'yellow']
 
+function getPosition(index) {
+  return (360 - (index * wheelAngle)) + 360
+}
+
+const AnimatedWheel = styled.div`
+  height: 120px;
+  transform-style: preserve-3d;
+  animation-iteration-count: 1;
+  animation-timing-function: linear;
+  animation-duration: 2s;
+  animation-fill-mode: forwards;
+  animation-name: ${p => p.animation};
+  animation-play-state: ${p => p.isPaused ? 'paused' : 'running'};
+  animation-delay: -${p => !p.isPaused ? (p.delay || 0) : 0}ms;
+`
+
 export function Wheel(props) {
-  const { delay, isPaused } = props;
-  const animationDelay = `-${0}ms`;
+  const { delay, isPaused, prevIndex, nextIndex } = props;
+
+  const animation = keyframes`
+    from {
+      transform: rotateX(${prevIndex ? getPosition(prevIndex) : prevIndex}deg)
+    }
+    to {
+      transform: rotateX(${getPosition(nextIndex)}deg)
+    }
+  ` 
 
   const wheelFaces = []
   for (let i = 0; i < WHEELFACES_PER_ROW; i++) {
     wheelFaces.push(
-      <WheelFace rotateX={wheelAngle * i} radius={WHEEL_RADIUS} color="green" text={i} />
+      <WheelFace rotateX={wheelAngle * i} radius={WHEEL_RADIUS} color="lightblue" text={i} />
     )
   }
 
-  return <div style={{ animationDelay }} className={cx(styles.wheel, { [styles.pause]: isPaused })}>{wheelFaces}</div>
+  return <AnimatedWheel delay={delay} isPaused={isPaused} animation={animation}>{wheelFaces}</AnimatedWheel>
 
   //     <WheelFace color="red"/>
   //     <WheelFace color="blue"/>
