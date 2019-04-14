06892c1f42c3c1089be700bb4cd80754cb708661
Vance Longwill <vancelongwill@gmail.com>
2019-04-15 02:48:14 +0300

    Updates

---
+++ precache-manifest.9ff47e77b808bb1d578bd8acba24df76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "376a9b783dc18a28fdda914b3efdd10f",
+    "url": "/react-fruitmachine/static/media/control-lever.376a9b78.svg"
+  },
+  {
+    "revision": "cd79a681e84ce1865714",
+    "url": "/react-fruitmachine/static/js/runtime~main.cd79a681.js"
+  },
+  {
+    "revision": "019f0de2f12722640d5d",
+    "url": "/react-fruitmachine/static/js/main.019f0de2.chunk.js"
+  },
+  {
+    "revision": "392189819860eca9cd3f",
+    "url": "/react-fruitmachine/static/js/1.39218981.chunk.js"
+  },
+  {
+    "revision": "019f0de2f12722640d5d",
+    "url": "/react-fruitmachine/static/css/main.07f2851c.chunk.css"
+  },
+  {
+    "revision": "7b9aaf9b2e155ab9202d8a1a7fbfc84c",
+    "url": "/react-fruitmachine/index.html"
+  }
+];
